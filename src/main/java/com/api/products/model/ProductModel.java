d0e59b78d3ab5d54f4ef29af1fd841ec86aacb11
rudzzz <rudsolza13@gmail.com>
2022-11-28 20:57:00 -0300

    code

---
+++ src/main/java/com/api/products/model/ProductModel.java
@@ -0,0 +1,37 @@
+package com.api.products.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class ProductModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String brand;
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+	
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}	
+}
