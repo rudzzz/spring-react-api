49b03be5e88aba32432c8f7a421a8865f1c0aa1c
rudzzz <rudsolza13@gmail.com>
2022-11-29 06:48:31 -0300

    class to handle request response added

---
+++ src/main/java/com/api/products/model/ResponseModel.java
@@ -0,0 +1,16 @@
+package com.api.products.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResponseModel {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
