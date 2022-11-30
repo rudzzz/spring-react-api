6878f6bd1b855314fa3bab9ece8b8d10c297d775
rudzzz <rudsolza13@gmail.com>
2022-11-30 07:07:38 -0300

    code

--- src/main/java/com/api/products/controller/ProductController.java
+++ src/main/java/com/api/products/controller/ProductController.java
@@ -2,15 +2,21 @@ package com.api.products.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.products.model.ProductModel;
+import com.api.products.model.ResponseModel;
 import com.api.products.services.ProductService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProductController {
 	
 	@Autowired
@@ -28,6 +34,16 @@ public class ProductController {
 	
 	@PostMapping("/register")
 	public ResponseEntity<?> register(@RequestBody ProductModel product){
-		return productService.register(product);
+		return productService.register(product, "register");
+	}
+
+	@PutMapping("/update")
+	public ResponseEntity<?> update(@RequestBody ProductModel product){
+		return productService.register(product, "update");
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<ResponseModel> delete(@PathVariable Long id){
+		return productService.delete(id);
 	}
 }
